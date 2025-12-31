e728d2e8cf7f34e01c66a30a23316635371e46d4
fengshutao <69944484+fengshutao@users.noreply.github.com>
2025-12-31 17:50:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,8 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # iStore
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
+git clone https://github.com/linkease/istore-ui package/luci-app-store
+git clone https://github.com/linkease/istore package/istore
 # echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed' >>feeds.conf.default
 # echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
